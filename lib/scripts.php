3a6159875d07ab4b5243a580801b58a2eedce790
John Hotovy <john.hotovy@gmail.com>
2013-12-22 13:14:45 -0600

    Style the signup form

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dfbc2053aa98816a3de8b3f53084820d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19135eedc78cb8e903e8a80fb9975ff2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
