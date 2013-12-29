e11a1e4a89a9a05e73c97ea462cb37f6a78a3fc7
John Hotovy <john.hotovy@gmail.com>
2013-12-29 14:10:48 -0600

    Update primary button color

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '753d6c4740f089b82d0136117c85e1bd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd24127cebec96069cbeff986d47886a0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
