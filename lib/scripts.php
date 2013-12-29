b0ec9db9df1b9c92ef09891f02b1e693ab61836c
John Hotovy <john.hotovy@gmail.com>
2013-12-29 15:40:10 -0600

    Additional styling on the blog page

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '80d267b7ba589b6fb9e4fdd6f4f95e6b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a001b26e33ca216e62a32c27bd206969');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content.php
+++ templates/content.php
@@ -1,10 +1,14 @@
 <article <?php post_class(); ?>>
-  <?php refined_featured_image('thumbnail', 'img-thumbnail pull-left margin-bottom-right'); ?>
   <header>
     <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
+  <?php refined_featured_image('thumbnail', 'pull-left margin-bottom-right'); ?>
   <div class="entry-summary">
-    <?php the_excerpt(); ?>
+    <?php
+      global $more;
+      $more = 0;
+      the_content();
+    ?>
   </div>
 </article>
