e0d2cdabf4356d3dda7a36901673665785e9cfa0
John Hotovy <john.hotovy@gmail.com>
2013-12-29 14:15:19 -0600

    Switch heading font to Helvetica instead of Helvetica Neue

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd24127cebec96069cbeff986d47886a0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c594860dc2bab8df3cf4bfc86661d08c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
