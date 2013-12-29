174dcb26d107c2f640cad95884438cc0217fe41f
John Hotovy <john.hotovy@gmail.com>
2013-12-29 16:12:16 -0600

    Style images in individual post pages

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a001b26e33ca216e62a32c27bd206969');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9d6b4578d16d1bcbd0f61e8795a5a2ef');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
