1f202e529d8a59bdd142b24d58b39330317186d3
John Hotovy <john.hotovy@gmail.com>
2013-12-29 11:38:42 -0600

    Remove drop shadow and rounded edges on cards

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0261644398b5f3a2fe77ec66310f3cbf');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c1a6e5a3cf8d55a5b8424330d5ce6383');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
