2b1858e156e1e42a68fe060fc0bdb1c4c3206565
John Hotovy <john.hotovy@gmail.com>
2014-01-14 17:17:43 -0600

    Style regular blockquotes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9d6b4578d16d1bcbd0f61e8795a5a2ef');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '38390d0955e86b819c0bc33983b57c74');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
