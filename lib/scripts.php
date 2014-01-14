f516b621104275186aef2964e88667705f5fb433
John Hotovy <john.hotovy@gmail.com>
2014-01-14 17:23:39 -0600

    Style start of article quotes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '38390d0955e86b819c0bc33983b57c74');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5c4cf2ad970d5bb19e36b8f1d3211dec');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
