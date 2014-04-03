b92c695d82235538adda30c0e60ec39f297c008f
John Hotovy <john.hotovy@gmail.com>
2014-04-03 18:08:59 -0500

    Hide profile details section during registration

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fbddf8282548d9343d205387eba9c1e3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '43ef36dbf58f58e2369f7cecff328510');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
