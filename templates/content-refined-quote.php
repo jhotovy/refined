194d9661a79b8d1b0a69b83dab3aaafb2a45e39f
John Hotovy <john.hotovy@gmail.com>
2014-04-04 14:56:03 -0500

    Make user submitted quotes clickable

--- templates/content-refined-quote.php
+++ templates/content-refined-quote.php
@@ -1,5 +1,7 @@
 <div class="masonry-item refined-user-quote">
-  <div class="quote"><i>&ldquo;<?php echo get_the_content(); ?>&rdquo;</i></div>
+  <div class="quote"><i><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+    &ldquo;<?php echo get_the_content(); ?>&rdquo;
+  </a></i></div>
   <p id="author">&#8212&nbsp<?php the_title(); ?></p>
   <hr/>
 </div>
