1d93cde2c207322db107856e425fdef39f80734a
John Hotovy <john.hotovy@gmail.com>
2014-04-04 15:28:06 -0500

    Use correct quote style on single quote page

--- templates/single-refined-quote.php
+++ templates/single-refined-quote.php
@@ -1,6 +1,6 @@
 <hr/>
 <div class="refined-user-quote">
-  <div class="quote"><i>"<?php echo get_the_content(); ?>"</i></div>
+  <div class="quote"><i>&ldquo;<?php echo get_the_content(); ?>&rdquo;</i></div>
   <div>
     <p id="author" class="pull-left">&#8212&nbsp<?php the_title(); ?></p>
     <p id="submitted-by" class="pull-right">
