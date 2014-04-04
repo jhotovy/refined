a132a2dea5c8d40f4b2efecbc10b21dd62b468ec
John Hotovy <john.hotovy@gmail.com>
2014-04-03 22:51:25 -0500

    Force page titles called "Forums" to be displayed as "Discussions"

--- lib/titles.php
+++ lib/titles.php
@@ -3,35 +3,40 @@
  * Page titles
  */
 function roots_title() {
+  $retval = '';
   if (is_home()) {
     if (get_option('page_for_posts', true)) {
-      return get_the_title(get_option('page_for_posts', true));
+      $retval = get_the_title(get_option('page_for_posts', true));
     } else {
-      return __('Latest Posts', 'roots');
+      $retval = __('Latest Posts', 'roots');
     }
   } elseif (is_archive()) {
     $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
     if ($term) {
-      return apply_filters('single_term_title', $term->name);
+      $retval = apply_filters('single_term_title', $term->name);
     } elseif (is_post_type_archive()) {
-      return apply_filters('the_title', get_queried_object()->labels->name);
+      $retval = apply_filters('the_title', get_queried_object()->labels->name);
     } elseif (is_day()) {
-      return sprintf(__('Daily Archives: %s', 'roots'), get_the_date());
+      $retval = sprintf(__('Daily Archives: %s', 'roots'), get_the_date());
     } elseif (is_month()) {
-      return sprintf(__('Monthly Archives: %s', 'roots'), get_the_date('F Y'));
+      $retval = sprintf(__('Monthly Archives: %s', 'roots'), get_the_date('F Y'));
     } elseif (is_year()) {
-      return sprintf(__('Yearly Archives: %s', 'roots'), get_the_date('Y'));
+      $retval = sprintf(__('Yearly Archives: %s', 'roots'), get_the_date('Y'));
     } elseif (is_author()) {
       $author = get_queried_object();
-      return sprintf(__('Author Archives: %s', 'roots'), $author->display_name);
+      $retval = sprintf(__('Author Archives: %s', 'roots'), $author->display_name);
     } else {
-      return single_cat_title('', false);
+      $retval = single_cat_title('', false);
     }
   } elseif (is_search()) {
-    return sprintf(__('Search Results for %s', 'roots'), get_search_query());
+    $retval = sprintf(__('Search Results for %s', 'roots'), get_search_query());
   } elseif (is_404()) {
-    return __('Not Found', 'roots');
+    $retval = __('Not Found', 'roots');
   } else {
-    return get_the_title();
+    $retval = get_the_title();
   }
+  if ($retval == 'Forums') {
+    return 'Discussions';
+  }
+  return $retval;
 }
--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
+  <title><?php echo roots_title() ?> | Refine The Mind</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
