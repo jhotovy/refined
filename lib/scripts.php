a9e60df7650f4e021bdba75c079479a9bb56d549
John Hotovy <john.hotovy@gmail.com>
2014-04-04 12:35:03 -0500

    Increase vertical spacing on the sidebar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0448e7afa621beb34df1977c74d72eb4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dc62ee0d9ae8ee8714a499b131251e1c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
