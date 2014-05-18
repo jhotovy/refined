8409301ab5c5b02dfc5e57a1aaca0a89db97820a
John Hotovy <john.hotovy@gmail.com>
2014-05-18 15:39:28 -0500

    Fix facebook share button getting cut off in sharebar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1d1ab60af5d0da30bf36de3aa33a9266');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '55c20e21b289754ec2cee73a097ea006');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
