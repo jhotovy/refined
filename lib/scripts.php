7891b7896dacef55b81e2f4ac83c80856a16591b
John Hotovy <john.hotovy@gmail.com>
2014-05-18 21:39:45 -0500

    Lighten the lavalamp for the main menu

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '91bb6286b07bc4c6636bbd2ba84e8eed');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd0fc4bb7c421056260880917be8e7e34');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
