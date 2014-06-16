c89e654e1fa9fb8634dde625940f0c74aa1dc676
John Hotovy <john.hotovy@gmail.com>
2014-06-15 23:39:02 -0500

    Adjust images page for mobile view

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '63ce96bc1784e267017e2deaf54f2ad4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19e0275417342ee648b160a3b2f83d72');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
