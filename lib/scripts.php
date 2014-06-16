9e8decede9709527a9bdf5b3992a56bce10f8431
John Hotovy <john.hotovy@gmail.com>
2014-06-15 22:04:25 -0500

    Add new logo to top of page

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b88c384ef6e0bb1fd60e488a7d69bfa1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '55de790dd97cc1fb4cccb1639867771a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
