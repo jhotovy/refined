060066087c627bf1b1b51034c9cf8924904c090e
John Hotovy <john.hotovy@gmail.com>
2014-06-26 22:51:11 -0500

    Show profile details since name is now required

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19e0275417342ee648b160a3b2f83d72');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cdeecaff44c4d2fc7971e5dde0a40fb3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
