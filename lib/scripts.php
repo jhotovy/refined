f7f64323aa2437c6ec4530ec07d98445a5f77151
John Hotovy <john.hotovy@gmail.com>
2014-07-23 22:24:48 -0500

    Fixed overflowing width for images on individual blog posts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd21a72719d6c9fea7fe0e8e066739bdb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6c75935e809eed1de2ba34bce33e7607');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
