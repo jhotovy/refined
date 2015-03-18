94a00e487b7df40dc660dc03dbc3be3c61f86e8b
John Hotovy <john.hotovy@gmail.com>
2015-03-17 19:45:17 -0500

    Rearrange posts on front page

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<?php query_posts(array('posts_per_page' => 6)); ?>
+<?php query_posts(array('posts_per_page' => 9)); ?>
 
 <div class="card padding-top">
   <?php if (!have_posts()) : ?>
@@ -8,14 +8,35 @@
   <?php endif; ?>
 
   <?php $counter = 0; ?>
-  <?php if (have_posts()) : echo '<div class="row">'; endif; ?>
-    <?php while (have_posts()) : the_post(); $counter++; ?>
-      <div class="col-sm-4">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="featured-post">
+        <?php the_post(); ?>
         <?php get_template_part('templates/content', 'front-post'); ?>
       </div>
-      <?php if ($counter % 3 == 0) : echo '</div><div class="vertical-gutter"></div><div class="row">'; endif; ?>
+    </div>
+    <div class="col-sm-6">
+      <div class="row">
+        <?php while ($counter < 4) : the_post(); $counter++; ?>
+          <div class="col-sm-6">
+            <?php get_template_part('templates/content', 'front-post'); ?>
+          </div>
+          <?php if ($counter == 2) : ?>
+            </div><div class="vertical-gutter"></div><div class="row">
+          <?php endif; ?>
+        <?php endwhile; ?>
+      </div>
+    </div>
+  </div>
+  <div class="vertical-gutter"></div>
+  <?php $counter = 0; ?>
+  <div class="row">
+    <?php while ($counter < 4) : the_post(); $counter++; ?>
+      <div class="col-sm-3">
+        <?php get_template_part('templates/content', 'front-post'); ?>
+      </div>
     <?php endwhile; ?>
-  <?php if ($counter > 0) : echo '</div>'; endif; ?>
+  </div>
 </div>
 <div class="vertical-gutter"></div>
 
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '668919770b138f32d223912a703da89e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '885860c3e317fdb2904befc19352307f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
