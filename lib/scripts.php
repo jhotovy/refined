0a759597e667b382e5199534f3f37a17539a13d4
John Hotovy <john.hotovy@gmail.com>
2015-04-30 15:19:55 -0500

    Make featured post look the same as other front page posts on mobile

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7388563c1fd2439ad3277eee22ac1d47');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '48b4319e1ffdefbabf1461dee7a92505');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
