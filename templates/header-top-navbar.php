2cd300d0b0053956030ef57738b0b8e2d84ff509
John Hotovy <john.hotovy@gmail.com>
2015-06-21 13:38:21 -0500

    Link header to home page

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,6 +1,6 @@
 <header class="logo-header chris-test">
   <div class="container">
-    <a><href="<?php echo get_home_url(); ?>"><img class="logo center-block" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" /></a>
+    <a href="<?php echo get_home_url(); ?>"><img class="logo center-block" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" /></a>
   </div>
 </header>
 <!-- chris johnson says hi -->
