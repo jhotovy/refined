8de1c676afb92a49c58e8f819314009d989231c9
John Hotovy <john.hotovy@gmail.com>
2015-06-21 13:24:00 -0500

    Update 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,26 @@
-<?php get_template_part('templates/page', 'header'); ?>
-
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+<br/>
+<div class="alert alert-info">
+  <?php _e('Sorry, that page seems not to exist. Here\'s a poem by Mary Oliver instead:', 'roots'); ?>
 </div>
 
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
+<p>
 
-<?php get_search_form(); ?>
+You do not have to be good.<br/>
+You do not have to walk on your knees<br/>
+for a hundred miles through the desert, repenting.<br/>
+You only have to let the soft animal of your body<br/>
+love what it loves.<br/>
+Tell me about despair, yours, and I will tell you mine.<br/>
+Meanwhile the world goes on.<br/>
+Meanwhile the sun and the clear pebbles of the rain<br/>
+are moving across the landscapes,<br/>
+over the prairies and the deep trees,<br/>
+the mountains and the rivers.<br/>
+Meanwhile the wild geese, high in the clean blue air,<br/>
+are heading home again.<br/>
+Whoever you are, no matter how lonely,<br/>
+the world offers itself to your imagination,<br/>
+calls to you like the wild geese, harsh and exciting –<br/>
+over and over announcing your place<br/>
+in the family of things.<br/>
+</p>
