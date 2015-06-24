af91b38cc98650240865e6ac4b82f7f85cd450ac
John Hotovy <john.hotovy@gmail.com>
2015-06-23 19:37:10 -0500

    Add spacing below images with black boxes & captions. Ahhh, that's better

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '51df278e733f12461781725ad8925013');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f81a08f7598386d2964c5eef26c88ea');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
