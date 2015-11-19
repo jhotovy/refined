14e2df471f326defce111cec41a95324b4668747
John Hotovy <john.hotovy@gmail.com>
2015-11-19 13:12:42 -0600

    Ensure images and <hr>'s don't overflow on mobile

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f81a08f7598386d2964c5eef26c88ea');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c2b79ed41faa71e1b92b7be759d1e75a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
